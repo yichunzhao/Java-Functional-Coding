cdc5a30960db1f8bda2b03230dd32b418fc17235
yichunzhao <zhaoyichun@hotmail.com>
2018-12-13 00:44:44 +0100

    primitive stream range; and box operator

--- src/test/java/DataModel/PersonTest.java
+++ src/test/java/DataModel/PersonTest.java
@@ -3,7 +3,6 @@ package DataModel;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
@@ -11,30 +10,6 @@ import static org.junit.Assert.assertTrue;
  * @author YNZ
  */
 public class PersonTest {
-
-    @Test
-    public void whenOnePersonIsAnother_ThenItReturnZero() {
-        Person p1 = new Person("mike", 36);
-        Person p2 = new Person("mike", 38);
-        int actual = p1.compareTo(p2);
-        assertEquals(actual, 0);
-    }
-
-    @Test
-    public void whenOnePersonIsNotAnother_LexicalLessThan_ThenItReturnLessThanZero() {
-        Person p1 = new Person("mike", 36);
-        Person p2 = new Person("tom", 38);
-        int actual = p1.compareTo(p2);
-        assertTrue(actual < 0);
-    }
-
-    @Test
-    public void whenOnePersonIsNotAnother_LexicalBiggerThan_ThenItReturnsBiggerThanZero() {
-        Person p1 = new Person("tony", 36);
-        Person p2 = new Person("mike", 38);
-        int actual = p1.compareTo(p2);
-        assertTrue(actual > 0);
-    }
 
     @Test
     public void whenTwoPerson_HavingSameNameDifferentAge_ThenItReturnFalse() {
